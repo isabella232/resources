edff82cbef8cd30ff52d1089fbeed033e66d6da1
ngervais <ngervais>
2006-09-29 18:34:52 +0000

    *** empty log message ***

--- scripts/resources_html.php
+++ scripts/resources_html.php
@@ -38,10 +38,10 @@ class ResourcesHtml {
 			<td colspan=4 class="tableHeaderTitle">Technical Resources</td>
 		</tr>
 		<tr>
-			<td width="50%" class="resourcesHeader" <a href="?<?=$filter->get_url_parameters('title')?>">Title<?=$this->get_sort_icon($filter, 'title')?></a></td>
+			<td width="50%" class="resourcesHeader" style="border-left:1px solid black;" <a href="?<?=$filter->get_url_parameters('title')?>">Title<?=$this->get_sort_icon($filter, 'title')?></a></td>
 			<td width="10%" class="resourcesHeader"><a href="?<?=$filter->get_url_parameters('type')?>">Type<?=$this->get_sort_icon($filter, 'type')?></a></td>
 			<td width="10%" class="resourcesHeader" <a href="?<?=$filter->get_url_parameters('date')?>">Date<?=$this->get_sort_icon($filter, 'date')?></a></td>
-			<td width="10%" class="resourcesHeader align="center">&nbsp;</td>
+			<td width="10%" class="resourcesHeader" style="border-right:1px solid black;" align="center">&nbsp;</td>
 		</tr>
 		</table>
 		<div class="resources">

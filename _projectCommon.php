e1073fdcb30f510a8410b803f8adc8835ad311d6
wbeaton <wbeaton>
2007-02-28 19:59:59 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -18,12 +18,6 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addNavSeparator("Resources", "index.php");
-	$Nav->addCustomNav("Documentation", "http://www.eclipse.org/documentation/", "_self", 1);
-	$Nav->addCustomNav("Articles", "/resources/index.php?type=article", "_self", 1);
-	$Nav->addCustomNav("Presentations", "/resources/index.php?type=presentation", "_self", 1);
-	$Nav->addCustomNav("Books", "/resources/index.php?type=book", "_self", 1);
-	$Nav->addCustomNav("Demonstrations", "/resources/index.php?type=demo", "_self", 1);
-	$Nav->addCustomNav("Code samples", "/resources/index.php?type=code", "_self", 1);
-	$Nav->addCustomNav("Services", "/services", "_self", 1);
+	$Nav->addCustomNav("Documentation", "/documentation/", "_self", 1);
 
 ?>

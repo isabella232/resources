f115bff4a01424a2c6f87b8f0aa54e776e58fd5f
wbeaton <wbeaton>
2007-02-28 16:30:51 +0000

    Added support to include author names.

--- podcasts.php
+++ podcasts.php
@@ -7,6 +7,7 @@ $filter->type = 'podcast';
 $filter->sortby = array('date');
 $resources = $Resources->get_resources($filter);
 
+// TODO Currently assumes that podcasts are in mp3 format. Need to support other types?
 foreach($resources as $podcast) {
   $mp3_link = null;
   foreach ($podcast->links as $link) {
@@ -19,6 +20,13 @@ foreach($resources as $podcast) {
   if (!$mp3_link) continue;
   
   $date = date("D, j M Y  12:00:00 \G\M\T", $mp3_link->get_date());
+  
+  $authors = ''; $separator = '';
+  foreach ($mp3_link->authors as $author) {
+    $authors .= $separator . $author->name;
+    $separator = ', ';
+  }
+  
   $tags = ''; $separator = '';
   foreach ($podcast->categories as $category) {
     $tags .= $separator . $category->title;
@@ -26,9 +34,9 @@ foreach($resources as $podcast) {
   }
 ?><item>
 	<title><?=$podcast->title?></title>
-	<itunes:author>Wayne Beaton</itunes:author>
+	<itunes:author><?= $authors ?></itunes:author>
 	<itunes:summary><?= $podcast->description ?></itunes:summary>
-	<enclosure url="<?= $mp3_link->path?>" type="audio/x-<?=$mp3_link->type?>" />
+	<enclosure url="<?= $mp3_link->path?>" type="audio/mpeg" />
 	<guid><?= $mp3_link->path?></guid>
 	<pubDate><?= $date ?></pubDate>
 	<itunes:keywords><?= $tags ?></itunes:keywords>

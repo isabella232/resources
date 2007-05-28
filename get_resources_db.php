adafbfcf173aac20f0789507bfd8855333a4609b
wbeaton <wbeaton>
2007-05-28 21:15:21 +0000

    *** empty log message ***

---
+++ get_resources_db.php
@@ -0,0 +1,49 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2006 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Wayne Beaton (Eclipse Foundation)- initial API and implementation
+ *******************************************************************************/
+
+	#*****************************************************************************
+	#
+	# get_resources.rss
+	#
+	# Author: 		Wayne Beaton
+	# Date:			2006-09-21
+	#
+	# Description:
+	#    This file retu
+	#****************************************************************************
+	
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
+
+$connection = new DBConnection();
+$connection->connect();
+
+dump_table("author");
+
+$connection->disconnect();
+
+function dump_table($name) {
+	$result = mysql_query("select * from $name");
+	while ($row = mysql_fetch_assoc($result)) {
+		$columns = array_keys($row);
+		
+		$col_names = '';
+		$col_values = '';
+		$separator = '';
+		foreach($columns as $column) {
+			$col_names .= $separator . $column;
+			$col_values .= $separator . "\"$row[$column]\"";
+			$separator=',';
+		}
+		echo "insert into $name ($col_names) values ($col_values);";
+	}
+}

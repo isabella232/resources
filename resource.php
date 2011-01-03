8eb30b2ce210cd0777ae9682cfa5660ec61261a8
wbeaton <wbeaton>
2011-01-03 03:24:48 +0000

    *** empty log message ***

--- resource.php
+++ resource.php
@@ -1,5 +1,4 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-/*******************************************************************************
+<?php  /*******************************************************************************
  * Copyright (c) 2006 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -26,7 +25,14 @@
 	#
 	#****************************************************************************
 
-	#
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+$App = new App();	
+$Nav = new Nav();	
+$Menu = new Menu();		
+include($App->getProjectCommon());    
+
 	# Begin: page-specific settings.  Change these. 
 	$pageKeywords	= "Eclipse, software development, resources, projects, talks";
 	$pageAuthor		= "Wayne Beaton";
@@ -40,17 +46,26 @@
 	# End: page-specific settings
 	#	
 	
+	/*
+	 * Get the value of the 'id' parameter. If it is not
+	 * a sequence of digits, bail out and redirect to the
+	 * resources root page.
+	 */
+	$id = $App->getHTTPParameter('id');
+	if (!preg_match('/\d+/', $id)) {
+		header ("Location: /resources");
+		exit;
+	}
+	
 	require_once("scripts/resources.php");
 	$resources = new Resources();
-	$id = $_GET['id'];
-	if (preg_match('/\d+/', $id)) 
-		$resource = $resources->get_resource($id);
+	$resource = $resources->get_resource($id);
 	if (!$resource) {
 		header ("Location: /resources");
 		exit;
 	}
 	
-	$pageTitle = "$resource->title";
+	$pageTitle = $resource->title;
 	$summary = $resources->get_resource_summary($resource);
 
 	$resources->dispose();

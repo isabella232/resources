906b6b7dfef306b808ab5214db42a41bce509d56
Wayne Beaton <wayne@eclipse.org>
2013-03-01 10:32:40 -0500

    Show promotions on the /resources page

--- _projectCommon.php
+++ _projectCommon.php
@@ -14,6 +14,7 @@
 	# See the Committer Tools "How Do I" for list of themes
 	# https://dev.eclipse.org/committers/ 
 	$theme = "Nova";
+	$App->Promotion = TRUE;
 
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
